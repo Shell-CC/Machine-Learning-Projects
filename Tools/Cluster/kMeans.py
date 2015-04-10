4af6b7a744ee61f637c97f3e3c45eaa0b44e7376
Xiaoxi Guo <xguo0122@gmail.com>
2015-04-09 20:00:30 -0500

    part of k-means

--- Tools/Cluster/kMeans.py
+++ Tools/Cluster/kMeans.py
@@ -4,10 +4,42 @@
 """
 
 import parseData as pd
+from random import sample
+import numpy as np
+
+def cluster(X, k):
+    X = np.asarray(X)
+    cent = kmeans(X, k)
+    return cent
+
+def kmeans(X, k):
+    numX = X.shape[0]
+    # random select k points as clustering centroids
+    centroids = sample(X, k)
+    # while cluaster changed
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        labels = np.zeros(numX)
+        minDist = np.empty(numX)
+        minDist.fill(np.Inf)
+        for cent in centroids:
+            # calculate the distance between the all data and the centroids
+            diff = np.tile(cent, (numX,1)) - X
+            dist = (diff**2).sum(axis=1)
+            minDist = np.minimum(dist, minDist)
+            # assign the point to the cluster with lowest distance
+            labelChange = dist-minDist
+            labelChange[labelChange>0]=-1
+            labelChange += 1
+            labels += labelChange
+        # update the centroids
+    return centroids
 
 def main():
     X = pd.textAsFloat('../testData/testKMeans.txt', None, '\t')
-    print X
+    cent = cluster(X, 4)
+    print cent
 
 if __name__ == '__main__':
     main()
