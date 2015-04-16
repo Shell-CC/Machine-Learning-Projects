78ef7618301ce60925faa3f6e1d13112fbe9cc2b
Xiaoxi Guo <xguo0122@gmail.com>
2015-04-15 22:09:04 -0500

    create pca

---
+++ Tools/Preprocess/DimReduct.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""This is implementaion of dimensionality reduction algorithms
+"""
+
+def pca(dataset):
+    
+    return dataset
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
