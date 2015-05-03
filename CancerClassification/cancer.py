59cdd1442d5d47f3722359c1fbc0ec717a8bb80f
Xiaoxi Guo <xguo0122@gmail.com>
2015-05-03 16:23:38 -0500

    select features using resub/3nn/exhaustive

--- CancerClassification/cancer.py
+++ CancerClassification/cancer.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
 import numpy as np
+import Classifier.kNN as knn
+import Classifier.errorEstimator as err
+from itertools import combinations
 
 def main():
     (Xtrain, ytrain, IDtrain, names) = readData('Training_Data.txt')
-    print Xtrain
-    print ytrain
-    print exaustiveSearch(Xtrain, [0,1])
+    """ Select 1-3 feature using exhaustive search based on resubstitutions of 3nn
+    """
+    feats = range(len(names))
+    featNum = [1, 2, 3]
+    for r in featNum:
+        minErr = 1.0;
+        for subsets in combinations(feats, r):
+            subX = exaustiveSearch(Xtrain, subsets)
+            model = knn.train(subX, ytrain, 3)
+            yhat = knn.classify(subX, model)
+            # print ytrain
+            # print yhat
+            error = err.resub(ytrain, yhat)
+            if error < minErr:
+                bestSubsets = np.asarray(subsets) + 1
+                minErr = error
+        print 'the best susbsets with %d features is %s, with error %f' % (r, bestSubsets, minErr)
+    # yhat = knn.test(subX, ytrain, model)
     (Xtest, ytest, IDtest, names) = readData('Testing_Data.txt')
 
 def readData(filename):
@@ -21,8 +39,8 @@ def readData(filename):
     f.close
     X = np.asarray(X)
     labels = np.asarray(labels)
-    print 'Read %d datas from %s.' % (len(IDs), filename)
-    print 'Each data contains %d genes, with a ID and a label.' % len(names)
+    # print 'Read %d datas from %s.' % (len(IDs), filename)
+    # print 'Each data contains %d genes, with a ID and a label.' % len(names)
     return X, labels, IDs, names
 
 def exaustiveSearch(X, subsets):
